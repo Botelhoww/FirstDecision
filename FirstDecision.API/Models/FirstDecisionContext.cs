33624b02c01651e7fbd1e559addc528adc3d74bb
Vinicius Botelho <vbawada@gmail.com>
2023-09-25 16:43:40 -0300

    adicionado biblioteca do Entity Framework e adicionado Context

---
+++ FirstDecision.API/Models/FirstDecisionContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstDecision.API.Models;
+
+public partial class FirstDecisionContext : DbContext
+{
+    public FirstDecisionContext(DbContextOptions<FirstDecisionContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Pessoa> Pessoa { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pessoa>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Pessoa__3214EC27898C14E2");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
