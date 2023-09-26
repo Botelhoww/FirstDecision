0d8f0d3c8aa89066bcb7b90118541e03923fa431
Vinicius Botelho <vbawada@gmail.com>
2023-09-26 00:16:32 -0300

    criado projeto de testes

---
+++ FirstDecision.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FirstDecision.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
