4882c5bc739d52765e01d8f4c1b97104e4f5b327
Vinicius Botelho <vbawada@gmail.com>
2023-09-26 14:08:42 -0300

    <tests>: criado mais testes unitarios

---
+++ FirstDecision.Tests/EmailTest.cs
@@ -0,0 +1,48 @@
+﻿using FirstDecision.Business.Validators;
+using FirstDecision.Model.Entities;
+using FluentValidation;
+
+namespace FirstDecision.Tests
+{
+    public class EmailTest
+    {
+        [Test]
+        [Description("Testa um Email válido")]
+        [TestCase("emailvalido@dominio.com")]
+        public void Deve_Validar_Email_Corretamente(string email)
+        {
+            // Arrange
+            var emailValidator = new EmailValidator();
+            var pessoa = new Pessoa { Email = email };
+
+            // Act
+            var resultado = emailValidator.Validate(pessoa, options =>
+            {
+                options.IncludeRuleSets("EmailValidator");
+            });
+
+            // Assert
+            Assert.IsTrue(resultado.IsValid);
+        }
+
+        [Test]
+        [Description("Testa um Email inválido")]
+        [TestCase("string")]
+        public void Nao_Deve_Permitir_Email_Invalido(string email)
+        {
+            // Arrange
+            var emailValidator = new EmailValidator();
+            var pessoa = new Pessoa { Email = email };
+
+            // Act
+            var ex = Assert.Throws<ValidationException>(() => emailValidator.Validate(pessoa, options =>
+            {
+                options.ThrowOnFailures();
+                options.IncludeRuleSets("EmailValidator");
+            }));
+
+            // Assert
+            Assert.IsTrue(ex.Message.Contains("O e-mail informado não é válido"));
+        }
+    }
+}
